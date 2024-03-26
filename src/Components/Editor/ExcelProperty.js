942493697ee70f125b2af3f3d955b5f75c90e6ce
PADDIPUSHPENDRA <sunnyrony3011@gmail.com>
2024-03-26 17:56:56 +0530

    Double click feature added

--- src/Components/Editor/ExcelProperty.js
+++ src/Components/Editor/ExcelProperty.js
@@ -2,19 +2,52 @@ import React, { useState } from 'react'
 
 function ExcelProperty() {
     const [data, setData] = useState([
-        ["A1", "B1", "C1"],
-        ["A2", "B2", "C2"],
-        ["A3", "B3", "C3"],
+        ['A1', 'B1', 'C1'],
+        ['A2', 'B2', 'C2'],
+        ['A3', 'B3', 'C3'],
+        ['A3', 'B3', 'C3'],
+        ['A3', 'B3', 'C3'],
     ])
+    const [selectedCell, setSelectedCell] = useState(null)
+    const [selectedCellForInput, setSelectedCellForInput] = useState(null)
+
+    const handleChange = (row, col, value) => {
+        const newData = [...data]
+        newData[row][col] = value
+        setData(newData)
+    }
+
+    const handleDoubleClick = (row, column) => {
+        setSelectedCellForInput({ row, column })
+    }
+    const handleSingleClick = (row, column) => {
+        setSelectedCell({ row, column })
+    }
 
     return (
         <div>
             <table>
                 <tbody>
-                    {data.map(item => (
-                        <tr>
-                            {item.map(innerItem => (
-                                <td><input type="text" value={innerItem} /></td>
+                    {data.map((item, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {item.map((innerItem, columnIndex) => (
+                                <td
+                                    onClick={() => handleSingleClick(rowIndex, columnIndex)}
+                                    onDoubleClick={() => handleDoubleClick(rowIndex, columnIndex)}
+                                    key={columnIndex}
+                                    style={{
+                                        backgroundColor: selectedCell && selectedCell.row === rowIndex
+                                            && selectedCell.column === columnIndex ? 'green' : 'transparent'
+                                    }}
+                                >{
+                                        (
+                                            selectedCellForInput && selectedCellForInput.row === rowIndex && selectedCellForInput.column === columnIndex
+                                                ? <input type="text"
+                                                    value={innerItem}
+                                                    onChange={(e) => handleChange(rowIndex, columnIndex, e.target.value)}
+                                                /> : innerItem
+                                        )
+                                    }</td>
                             ))}
                         </tr>
                     ))}
