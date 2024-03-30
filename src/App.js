4e0cd90fa46656798da36d028431d826fe10dbc6
PADDIPUSHPENDRA <sunnyrony3011@gmail.com>
2024-03-30 11:58:57 +0530

    CountDown app completed

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import ExcelApp from './Components/Editor/ExcelApp';
 import ExcelProperty from './Components/Editor/ExcelProperty';
 import Table from './Components/Tables/Table';
 import Form from './Components/formFolde/Form';
+import CounterDown from './Components/Counter/Countdown';
 
 function App() {
   return (
     <div className="App">
       <p>Hello codder 🐻</p>
-      <ExcelProperty />
+      <CounterDown />
     </div>
   );
 }
--- src/Components/Counter/Countdown.js
+++ src/Components/Counter/Countdown.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 const Countdown = () => {
     const [timeLeft, setTimeLeft] = useState(10)
     const [stopStart, setStopStart] = useState(false)
+    const [message, setMessage] = useState('')
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -11,14 +12,28 @@ const Countdown = () => {
         if (timeLeft === 0 || stopStart) {
             clearInterval(timer)
         }
+        if (timeLeft < 5 && timeLeft > 0) {
+            setMessage("Your last 5 seconds left")
+        } else if (timeLeft === 0) {
+            setMessage('')
+        }
         return () => clearInterval(timer)
     }, [timeLeft, stopStart])
+
+    const formatTime = (time) => {
+        const min = Math.floor(time / 60)
+        const sec = time % 60
+        return `${min}: ${sec < 10 ? '0' : ''} ${sec}`
+    }
 
     return (
         <div>
             <h1>Time Counter</h1>
-            <div>Time Left :- {timeLeft}</div>
+            <div>Time Left :- {formatTime(timeLeft)}</div>
             <button onClick={() => setStopStart(!stopStart)} >{stopStart ? "Start" : "Stop"}</button>
+            <button onClick={() => setTimeLeft(0)} >Reset</button>
+            <div>{message}</div>
+            <input type="number" onChange={(e) => setTimeLeft(e.target.value)} />
         </div >
     );
 };
